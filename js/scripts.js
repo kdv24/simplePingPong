15060850d1fbf45f8c89c2593dc760e079d2bf4a
Kelly <kdv@me.com>
2015-01-16 10:15:05 -0800

    Javascript code fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,43 @@
-
 $(document).ready(function() {
-	//submitOK = "true";
-	$(function() {
-		$("#goButton").click(function() {
-			$(".userForm").show();
-		});
-		$('#userEntry form').submit(function(event) {	
-			var userInput = $('#userEntry').val();
+	$('#goButton').click(function() {
+	var userInput = prompt('What number do you choose?', 'number');
+	//alert('You chose the number ' + userInput);
 
-			$('ul').text(userInput);
-			$('.resultsOutput').show();
-			event.preventDefault();
-		});	
+	for (var counter=1; counter <= userInput; counter++){
+		if ((counter%3 == 0) && (counter%5 == 0)) {
+		  	console.log(counter + " ping-pong");
+		}
+		else if (counter%3 == 0) {
+			console.log(counter + " ping");
+		}
+		else if (counter%5 == 0) {
+			console.log(counter + " pong");
+		}	
+		else {
+			console.log(counter);
+		}
+	};	
+
+
 	});
-
+});
 
 	
+
+
+
 	//	$(".resultsOutput").text(userInput);
 	//  add response for just in case not integer
 	//  (eg, if not integer, give new prompt)
 	//	if (isNaN (userInput)) {
-	//		alert("Oops, try again.  You need to enter a number.");
-	//		submitOK = "false";	
+	//		alert("Oops, try again.  You need to enter a number.");	
 	//	}		
-	//	if (submitOK == "false") {
-	//		return false;
+	//	
 	//	}
-});
 
 
 
-/*	for (var n=0; n <= userInput; n++){
-		if ((userInput%3 == 0) && (userInput%5 == 0)) {
-		  	console.log("ping-pong");
-		}
-		if (userInput%3 == 0) {
-			console.log("ping");
-		}
-		if (userInput%5 == 0) {
-			console.log("pong");
-		}	
-		else {
-			console.log(n)
-		}
-	}	
+/*******CODE BELOW WORKS IN JS CONSOLE************/
+
 
 //	$(".results").show();*/
