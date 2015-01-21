0794132c6422c2d4148300dfea5a7fe9a70c7391
Kelly <kdv@me.com>
2015-01-21 13:35:45 -0800

    fixing files, I hope

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,23 @@ $(document).ready(function() {
 	var userInput = prompt('What number do you choose?', 'number');
 	//alert('You chose the number ' + userInput);
 
-	for (var counter=1; counter <= userInput; counter++){
-		if ((counter%3 == 0) && (counter%5 == 0)) {
-		  	console.log(counter + " ping-pong");
-		}
-		else if (counter%3 == 0) {
-			console.log(counter + " ping");
-		}
-		else if (counter%5 == 0) {
-			console.log(counter + " pong");
-		}	
-		else {
-			console.log(counter);
-		}
-	};	
-
-
+	while (isNaN(userInput)) {
+		if (isNaN(userInput)) {
+				alert("Oops, try again.  You need to enter a number.");	
+				userInput = prompt('What number do you choose?', 'number');
+		}; 
+	};			
+			for (var counter=1; counter <= userInput; counter++){						
+				if ((counter%3 == 0) && (counter%5 == 0)) {
+				  	document.write(counter + " ping-pong" + "<br>");
+				} else if (counter%3 == 0) {
+					document.write(counter + " ping" + '<br>');
+				} else if (counter%5 == 0) {
+					document.write(counter + " pong" + '<br>');
+				} else {
+					document.write(counter + '<br>');
+				}
+			};	
 	});
 });
 
@@ -26,18 +27,5 @@ $(document).ready(function() {
 
 
 
-	//	$(".resultsOutput").text(userInput);
-	//  add response for just in case not integer
-	//  (eg, if not integer, give new prompt)
-	//	if (isNaN (userInput)) {
-	//		alert("Oops, try again.  You need to enter a number.");	
-	//	}		
-	//	
-	//	}
 
 
-
-/*******CODE BELOW WORKS IN JS CONSOLE************/
-
-
-//	$(".results").show();*/
